08650c9fbef494cf29e32e8766de61758c202c52
Jakub Malik <malik.j@poczta.fm>
2024-11-22 19:33:03 +0100

    Aktualizacji GUI, aby była możliwość wyboru wielkości planszy oraz ilości min, a także czasu opóźnienia w ruchach AI. Dodanie wyświetlania obecnie używanego algorytmu, podświetlania wybranego pola, a także zapis ruchów z gry.

--- ai_player.py
+++ ai_player.py
@@ -7,35 +7,38 @@ class AIPlayer:
         self.game = game
         self.neighbour_deduction = NeighbourDeduction(game)
         self.cluster_inference = ClusterInference(game)
+        self.current_algorithm = None
 
     def make_move(self):
         move = self.neighbour_deduction.find_move()
         if move:
+            self.current_algorithm = "Neighbour Deduction"
             action, row, col = move
-            print("AI znalazło ruch dedukcyjny (Neighbour Deduction).")
-            return (action, row, col)
+            print(f"AI znalazło ruch dedukcyjny ({self.current_algorithm}).")
+            return action, row, col
 
         move = self.cluster_inference.analyze_clusters()
         if move:
+            self.current_algorithm = "Cluster Inference"
             action, row, col = move
-            print("AI znalazło ruch dedukcyjny (Cluster Inference).")
-            return (action, row, col)
+            print(f"AI znalazło ruch dedukcyjny ({self.current_algorithm}).")
+            return action, row, col
 
         print("AI nie znalazło bezpiecznego ruchu, wykonuje losowy ruch.")
+        self.current_algorithm = "Random Move"
         random_move = self.get_random_move()
         if random_move:
             row, col = random_move
             print(f"AI wykonuje losowy ruch na ({row}, {col}).")
-            return ('reveal', row, col)
+            return 'reveal', row, col
         return None
 
     def get_random_move(self):
-        available_moves = []
-        for row in range(self.game.rows):
-            for col in range(self.game.cols):
-                if not self.game.revealed[row][col] and not self.game.flags[row][col]:
-                    available_moves.append((row, col))
+        available_moves = [
+            (row, col) for row in range(self.game.rows) for col in range(self.game.cols)
+            if not self.game.revealed[row][col] and not self.game.flags[row][col]
+        ]
+        return random.choice(available_moves) if available_moves else None
 
-        if available_moves:
-            return random.choice(available_moves)
-        return None
+    def get_current_algorithm(self):
+        return self.current_algorithm
